b88170932576b6a7b95cb17e1f8dd21d5b0f2a6f
Ailton Zacarias <ailton@f4consult.com.br>
2019-12-23 16:48:31 -0300

    Add files via upload

---
+++ Freemind/091113-1224_Controle.mm
@@ -0,0 +1,58 @@
+<map version="0.7.1">
+<node ID="_Freemind_Link_714570359" TEXT="Controle de ca&#xe7;ambas">
+<node ID="_" TEXT="Contrato Padr&#xe3;o de Presta&#xe7;&#xe3;o de Servi&#xe7;os" POSITION="right"/>
+<node ID="Freemind_Link_877482105" TEXT="Clientes" POSITION="right">
+<node ID="Freemind_Link_1734383690" TEXT="Cadastros">
+<node ID="Freemind_Link_351726845" TEXT="Clientes">
+<node ID="Freemind_Link_33636645" TEXT="Obras"/>
+</node>
+<node ID="Freemind_Link_1564091709" TEXT="Motoristas"/>
+<node ID="Freemind_Link_1814834144" TEXT="Aterros">
+<node ID="Freemind_Link_1923192128" TEXT="Cotas"/>
+</node>
+<node ID="Freemind_Link_1653067092" TEXT="Transportador"/>
+</node>
+<node ID="Freemind_Link_1017635246" TEXT="Categorias">
+<node ID="Freemind_Link_1715698361" TEXT="Propaganda">
+<node ID="Freemind_Link_1216646745" TEXT="Ca&#xe7;amba"/>
+<node ID="Freemind_Link_185497529" TEXT="Indica&#xe7;&#xe3;o">
+<node ID="Freemind_Link_1150669971" TEXT="Quem?"/>
+</node>
+<node ID="Freemind_Link_1805709084" TEXT="Site"/>
+<node ID="Freemind_Link_38924344" TEXT="Etc..."/>
+</node>
+<node ID="Freemind_Link_314150063" TEXT="Motivos de retorno">
+<node ID="Freemind_Link_428058462" TEXT="Faixa de pedestre"/>
+<node ID="Freemind_Link_1463292954" TEXT="Local de esquina"/>
+<node ID="Freemind_Link_175073465" TEXT="Restri&#xe7;&#xe3;o de hor&#xe1;rio"/>
+<node ID="Freemind_Link_1535144418" TEXT="Etc..."/>
+</node>
+<node ID="Freemind_Link_719016974" TEXT="Status da viagem">
+<node ID="Freemind_Link_267601464" TEXT="ok"/>
+</node>
+<node ID="Freemind_Link_125390773" TEXT="Veiculos"/>
+<node ID="Freemind_Link_61073681" TEXT="Fechamentos">
+<node ID="Freemind_Link_772548552" TEXT="Quinzenal"/>
+<node ID="Freemind_Link_1483582310" TEXT="Semanal"/>
+<node ID="Freemind_Link_1800122117" TEXT="Mensal"/>
+</node>
+</node>
+<node ID="Freemind_Link_832700196" TEXT="Controles">
+<node ID="Freemind_Link_176356883" TEXT="Dias para Atraso (Ap&#xf3;s o vencimento)"/>
+<node ID="Freemind_Link_1015454541" TEXT="Multa por dia de atraso (%)"/>
+</node>
+<node ID="Freemind_Link_90720243" TEXT="Liga&#xe7;&#xf5;es (Solicita&#xe7;&#xf5;es de Servi&#xe7;o)"/>
+<node ID="Freemind_Link_1812412364" TEXT="Roteiros (Realiza&#xe7;&#xe3;o do servi&#xe7;o)">
+<node ID="Freemind_Link_744024321" TEXT="Viagens"/>
+<node ID="Freemind_Link_837534450" TEXT="Impressos">
+<node ID="Freemind_Link_991454529" TEXT="Roteiros"/>
+<node ID="Freemind_Link_845297424" TEXT="Pedidos (Liga&#xe7;&#xf5;es)"/>
+<node ID="Freemind_Link_829442950" TEXT="CTR"/>
+</node>
+</node>
+<node ID="Freemind_Link_1710149889" TEXT="Cobran&#xe7;a">
+<node ID="Freemind_Link_1881063768" TEXT="Emiss&#xe3;o de boletos"/>
+</node>
+</node>
+</node>
+</map>
